728fca83109b3fbb88424c9e42f736ff3d3ae9e8
LVALL <valeradracenko418@gmail.com>
2020-11-14 17:52:25 +0200

    html_maker functionality done

---
+++ html_maker.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'html_maker'
+  s.version     = '0.0.4'
+  s.date        = '2020-11-14'
+  s.summary     = "Make html"
+  s.description = "Simple html maker"
+  s.authors     = ["Valerii Drachenko"]
+  s.email       = 'valeradracenko418@gmail.com'
+  s.files       = ["lib/html_maker.rb"]
+  s.homepage    = ''
+  s.license     = 'MIT'
+end
---
+++ lib/html_maker.rb
@@ -0,0 +1,20 @@
+class MakeHtml
+  def make_html(content, bypass_html, file_name = 'index.html')
+    markup = content.gsub!(/[<>]/, '') if bypass_html == false
+    markup = content unless bypass_html == false
+
+    f = File.new(file_name, 'w+')
+    f.puts "<!DOCTYPE html>"
+    f.puts "  <head>"
+    f.puts "    <meta charset='utf-8'>"
+    f.puts "    <title>My Tamago</title>"
+    f.puts "  </head>"
+    f.puts "  <body>"
+    f.puts "    <div>"
+    f.puts "      #{markup}"
+    f.puts "    </div>"
+    f.puts "  </body>"
+    f.puts "</html>"
+    f.close
+  end
+end
